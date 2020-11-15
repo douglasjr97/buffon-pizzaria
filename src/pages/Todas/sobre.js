a5d22bfaaedade4e131ebcfdd291a0ef9e4c97d9
Douglas Falcao <falcao.douglas97@gmail.com>
2020-11-15 01:15:50 -0300

    First commit

---
+++ src/pages/Todas/sobre.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Sobre extends Component{
+     render(){
+        return(
+            <div>
+                Sobre<br/>
+                
+                <hr/>
+            </div>         
+        );
+    }
+}
+
+export default Sobre;
